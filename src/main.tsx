3348983f1050715edaaa2cd4b0285c25ddec4805
pashka1837 <pashka1837@yandex.kz>
2025-03-08 00:55:02 +0700

    feat:  strict mode removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@fontsource/inter/index.css";
 import "./index.css";
@@ -7,8 +6,4 @@ import { makeServer } from "./__mock/server";
 
 makeServer();
 
-createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
-  <App />
-  // </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
